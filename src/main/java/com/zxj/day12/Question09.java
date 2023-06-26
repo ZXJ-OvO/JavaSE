91a39d3b4d9fe6292dbb4b15593500c6a33568a3
Epiphany <zxjOvO@gmail.com>
2023-06-26 16:12:35 +0800

    day12 ppt schoolwork

--- src/main/java/com/zxj/day12/Question09.java
+++ src/main/java/com/zxj/day12/Question09.java
@@ -2,6 +2,13 @@ package com.zxj.day12;
 
 public class Question09 {
     public static void main(String[] args) {
+        Question09 question09 = new Question09();
+        question09.eat();
 
     }
+
+    public void eat() {
+        System.out.println("吃饭!");
+    }
+
 }
