43e2b29d144efdff632e3d65064c2b788ae89db1
Epiphany <zxjOvO@gmail.com>
2023-07-06 16:16:15 +0800

    day19 缓冲流

--- src/main/java/com/zxj/day19/Question04.java
+++ src/main/java/com/zxj/day19/Question04.java
@@ -1,4 +1,26 @@
 package com.zxj.day19;
 
+import java.io.*;
+
+/**
+ * 字节缓冲流：包装原始流后自带8kb的缓冲池
+ */
 public class Question04 {
+    public static void main(String[] args) {
+        try (
+                InputStream inputStream = new FileInputStream("src/main/resources/test.txt");
+                InputStream bufferedInputStream = new BufferedInputStream(inputStream);
+                OutputStream outputStream = new FileOutputStream("src/main/resources/aa/tt/test.txt");
+                OutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
+        ) {
+            byte[] bytes = new byte[1024];
+            int len;
+            while ((len = bufferedInputStream.read()) != -1) {
+                bufferedOutputStream.write(bytes, 0, len);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/com/zxj/day19/Question05.java
+++ src/main/java/com/zxj/day19/Question05.java
@@ -1,4 +1,27 @@
 package com.zxj.day19;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+/**
+ * 字符缓冲流
+ */
 public class Question05 {
+    public static void main(String[] args) {
+        try (
+                BufferedReader bufferedReader = new BufferedReader(new FileReader("src/main/resources/test.txt"));
+                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("src/main/resources/aa/tt/test.txt"));
+        ) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                bufferedWriter.write(line);
+                bufferedWriter.newLine();
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
