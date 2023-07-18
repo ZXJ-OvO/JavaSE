81287ca280b529772789f67081194a420485d8ba
Epiphany <zxjOvO@gmail.com>
2023-07-18 20:56:23 +0800

    hutool math util

---
+++ src/main/java/com/hutool/math/MathUtil.java
@@ -0,0 +1,13 @@
+package com.hutool.math;
+
+import cn.hutool.core.math.Calculator;
+import com.customer.BigDecimalUtil;
+
+public class MathUtil {
+    public static void main(String[] args) {
+
+        // 计算指定表达式的值
+        System.out.println(Calculator.conversion("10*2+5*34/2"));
+
+    }
+}
---
+++ src/main/java/com/hutool/math/MoneyUtil.java
@@ -0,0 +1,14 @@
+package com.hutool.math;
+
+import cn.hutool.core.math.MathUtil;
+
+public class MoneyUtil {
+    public static void main(String[] args) {
+
+        // 金额元转分 long cent = MathUtil.yuanToCent(double yuan)
+        System.out.println(MathUtil.yuanToCent(1.2));
+
+        // 金额分转元 double yuan = MathUtil.centToYuan(long cent)
+        System.out.println(MathUtil.centToYuan(120));
+    }
+}
